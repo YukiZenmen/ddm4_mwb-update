011a9bbf71fe2f8738a08f9ab1b1ea6c39e80a66
YukiZenmen <101950109+YukiZenmen@users.noreply.github.com>
2023-02-23 12:08:50 -0500

    Delete mwb ddm4v5 update directory

--- mwb ddm4v5 update/lua/weapons/mg_base/modules/attachmentss/m4v5/barrels/attachment_vm_ar_m4v5_barrel.lua
+++
@@ -1,2 +0,0 @@
-ATTACHMENT.Base = "att_barrel"
---ATTACHMENT.Model = Model("models/viper/mw/attachments/mike4/attachment_vm_ar_mike4_barrel.mdl")
--- mwb ddm4v5 update/lua/weapons/mg_base/modules/attachmentss/m4v5/mags/attachment_vm_ar_m4v5_mag.lua
+++
@@ -1,2 +0,0 @@
-ATTACHMENT.Base = "att_magazine"
---ATTACHMENT.Model = Model("models/viper/mw/attachments/mike4/attachment_vm_ar_mike4_mag.mdl")
